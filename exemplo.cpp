7e30a38dae80028e81be3cba2592971b1f16a6fd
kaisy32 <140452913+user07777@users.noreply.github.com>
2024-11-19 12:34:19 -0300

    Update exemplo.cpp

--- exemplo.cpp
+++ exemplo.cpp
@@ -4,15 +4,23 @@
 
 int main()
 {
-	if (localStorage::get("user") == "") {
+	auto L = std::make_shared<localStorage>();
+	if (L.get()->get() == "") {
 		std::string user;
 		std::cout << "Digite seu usuario:";
 		std::cin >> user;
 
-		localStorage::save("user", localStorage::crypt::enc(user, "0xC0FFE"));
-		localStorage::swap(localStorage::appData_()); // salva no .exe
+		L->save("user", L->enc(user, "0xC0FFE")); //salva na memoria
+		L->save("pwd",  L->enc("senha", "0xC0FFE"));//salva na memoria
+
+		L->saveExe();
+		L->swap(); // salva no .exe
 	}
 	else {
-		std::cout << "ola," << localStorage::crypt::dec(localStorage::get("user"), "0xC0FFE") << "\n";
+		std::string pwd;
+		std::cout << "Ola," << L->dec(L->get("user"),"0xC0FFE") << " digite sua senha\n";
+		std::cin >> pwd;
+		if (L->dec(L->get("pwd"), "0xC0FFE") == pwd)
+			std::cout << "logado";
 	}
 }
